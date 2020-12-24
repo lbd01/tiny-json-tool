1c124a19ef82d2f3f97d0cfa776b7cca542c9167
lbd01 <lbd01@protonmail.com>
2020-12-25 00:33:36 +0100

    Readme update

--- src/test/java/net/lbd01/tiny/json/test/ReadmeCode.java
+++ src/test/java/net/lbd01/tiny/json/test/ReadmeCode.java
@@ -4,48 +4,40 @@ import net.lbd01.tiny.json.JsonObj;
 import net.lbd01.tiny.json.JsonVal;
 import net.lbd01.tiny.json.TinyJsonTool;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+
 import static org.junit.Assert.assertTrue;
 
 public class ReadmeCode {
 
     public static void main(String[] args) {
-        String json = "{\"a\":null,\"b\":1,\"c\":2.3,\"d\":false,\"e\":\"txt\",\"f\":[1],\"g\":{\"h\":1}}";
+        String jsonStr = "{\"a\":null,\"b\":1,\"c\":2.3,\"d\":false,\"e\":\"txt\",\"f\":[1],\"g\":{\"h\":1}}";
 
         //parse json
-        JsonObj node = TinyJsonTool.parseObject(json);
+        JsonObj json = TinyJsonTool.parseObject(jsonStr);
 
         //access json object fields
-        JsonVal val = node.get("a");
-        System.out.println("Field a is null: " + (val==null));
+        System.out.println("Field a is null: " + (json.get("a")==null));
+        System.out.println("Field b val: " + json.getInteger("b"));
+        System.out.println("Field c val: " + json.getDecimal("c"));
+        System.out.println("Field d val: " + json.getBool("d"));
+        System.out.println("Field e val: " + json.getString("e"));
+        System.out.println("Field f val: " + json.getArray("f").get(0).getInteger());
+        System.out.println("Field h val: " + json.getObject("g").getInteger("h"));
 
-        val = node.get("b");
-        System.out.println("Field b is int: " + val.isInteger());
-        System.out.println("Field b val: " + val.getInteger());
-
-        val = node.get("c");
-        System.out.println("Field c is decimal: " + val.isDecimal());
-        System.out.println("Field c val: " + val.getDecimal());
-
-        val = node.get("d");
-        System.out.println("Field d is bool: " + val.isBool());
-        System.out.println("Field d val: " + val.getBool());
-
-        val = node.get("e");
-        System.out.println("Field e is str: " + val.isString());
-        System.out.println("Field e val: " + val.getString());
-
-        val = node.get("f");
-        System.out.println("Field f is array: " + val.isArray());
-        System.out.println("Field f item is int: " + val.getArray().get(0).isInteger());
-        System.out.println("Field f val: " + val.getArray().get(0).getInteger());
-
-        val = node.get("g");
-        assertTrue("Wrong field g type", val.isObject());
-        System.out.println("Field h is int: " + val.getObject().get("h").isInteger());
-        System.out.println("Field h val: " + val.getObject().get("h").getInteger());
+        //create new json
+        JsonObj newJson = new JsonObj();
+        newJson.setNull("a");
+        newJson.set("b",1);
+        newJson.set("c",new BigDecimal("2.3"));
+        newJson.set("d",false);
+        newJson.set("e","txt");
+        newJson.setArrayOfIntegers("f", Arrays.asList(1));
+        newJson.set("g",new JsonObj().set("h",1));
 
         //serialize to json
-        System.out.println("json: " + node.toJson());
+        System.out.println("json: " + newJson.toJson());
     }
 
 }
